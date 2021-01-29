2d56d16fedab43fc74922c5686e9624fe4cbf07e
illia12 <goncharovphp@gmail.com>
2021-01-29 09:58:20 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "@/assets/scss/main.scss";
 
 Vue.config.productionTip = false;
 
-import ElementUI from "element-ui";
 Vue.use(ElementUI);
-
-import "element-ui/lib/theme-chalk/index.css";
-import "@/assets/scss/main.scss";
 
 new Vue({
   router,
